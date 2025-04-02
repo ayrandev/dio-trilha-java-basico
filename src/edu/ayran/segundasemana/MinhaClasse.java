a5e05c6d66a9e8eb1c89bb1a0f6e03afbc687708
Ayran <ayranvieira29@gmail.com>
2025-04-01 21:35:52 -0300

    Subindo repositorios para estudo de java

---
+++ src/edu/ayran/segundasemana/MinhaClasse.java
@@ -0,0 +1,14 @@
+package edu.ayran.segundasemana;
+public class MinhaClasse {
+    public static void main (String [] args) {
+        String primeiroNome = "Ayran";
+        String segundoNome = "Vieira";
+
+        String nomeCompleto = nomeCompleto(primeiroNome, segundoNome);
+
+        System.out.println(nomeCompleto);
+    }
+    public static String nomeCompleto (String primeiroNome, String segundoNome){
+        return "Resultado do método " primeiroNome.concat(" ").concat(segundoNome);
+    }
+}
