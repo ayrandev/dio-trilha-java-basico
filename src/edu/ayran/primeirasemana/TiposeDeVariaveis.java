55739df51d4c56bf4dcb5683c27233e6dbfe0f8c
Ayran <ayranvieira29@gmail.com>
2025-04-02 14:01:46 -0300

    Tipos de variaveis

---
+++ src/edu/ayran/primeirasemana/TiposeDeVariaveis.java
@@ -0,0 +1,25 @@
+package edu.ayran.primeirasemana;
+
+public class TiposeDeVariaveis {
+    public static void main (String[]args) {
+        double salarioMinimo = 1500;
+
+        short numeroCurto = 1;
+        int numeroNormal = numeroCurto;
+        
+        int numero = 10;
+
+        final double VALOR_DE_PI = 3.14;
+
+        System.out.print(VALOR_DE_PI);
+
+    }
+}
+
+        //int - Mais usado.
+        //byte
+        //long
+        //boolean
+        //float
+        //short
+        //double - Mais usado.
